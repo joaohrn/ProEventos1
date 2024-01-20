75404fa194bc5a4cfd1c61f3dafedbda2f0936ad
joaohrn0 <joaohrn0@gmail.com>
2024-01-20 11:36:56 -0300

    Adiciona frontend em Angular

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos : any;
+
+  constructor(private http: HttpClient) { }
+
+  public getEventos(): void{
+    this.http.get("https://localhost:5001/api/eventos").subscribe(
+      res => this.eventos = res,
+      error => console.log(error)
+    )
+  }
+  ngOnInit(): void {
+    this.getEventos()
+  }
+
+}
