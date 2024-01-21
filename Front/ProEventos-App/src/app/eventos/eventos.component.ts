6918f9596a004ec84e97d78db8a7c03d02d8ef9c
joaohrn0 <joaohrn0@gmail.com>
2024-01-21 19:03:40 -0300

    Seção 5

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,16 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any;
+  public eventos : any = [];
+  public eventosFiltrados: any = [];
+  public showImages = false;
+  private _filtroLista: string = "";
+
+
+  public get filtroLista() : string {
+    return this._filtroLista
+  }
+  
+  public set filtroLista(v : string) {
+    this._filtroLista = v;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+  
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase()
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+  
 
   constructor(private http: HttpClient) { }
 
   public getEventos(): void{
-    this.http.get("https://localhost:5001/api/eventos").subscribe(
-      res => this.eventos = res,
+      this.http.get("https://localhost:5001/api/eventos").subscribe(
+      res => {
+        this.eventos = res
+        this.eventosFiltrados = res
+      },
       error => console.log(error)
     )
   }
+  
   ngOnInit(): void {
     this.getEventos()
   }
